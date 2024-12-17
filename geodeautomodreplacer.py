46ae2f43a83189bfaf60d74d192b3a7f5c532c8a
skm150214 <159707063+skm150214@users.noreply.github.com>
2024-12-17 18:34:19 +0100

    Update geodeautomodreplacer.py

--- geodeautomodreplacer.py
+++ geodeautomodreplacer.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 SET_YOUR_OWN_GD_MODS_DIR =  "C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\geode\mods\ " #default #change for your own dir
-SET_YOUR_MOD_DIR = "example_mod\build\ " #replace for your own mod dir and build
+SET_YOUR_MOD_DIR = "C:\Users\User123\example_mod\build\ " #replace for your own mod dir and build
 SET_YOUR_GD_MOD_NAME = "example_mod_name.geode" #replace for your own mod name
 os.remove(SET_YOUR_OWN_GD_MODS_DIR + SET_YOUR_GD_MOD_NAME)
 shutil.copy(SET_YOUR_MOD_DIR + SET_YOUR_GD_MOD_NAME, SET_YOUR_OWN_GD_MODS_DIR + SET_YOUR_GD_MOD_NAME)
